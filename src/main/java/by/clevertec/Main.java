6e4821612cf7cce9faeab4474ebed3946aacc9de
Dmitry Gornev <gornev@yandex.ru>
2025-05-14 22:48:27 +0500

    Тестовый коммит

--- src/main/java/by/clevertec/Main.java
+++ src/main/java/by/clevertec/Main.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Задачки...");
         task1();
         task2();
         task3();
